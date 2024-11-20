f162670e6aa84f60a052cf27d6bee3df3c5dfdd1
arielfelippi <41402358+arielfelippi@users.noreply.github.com>
2024-11-19 21:58:12 -0300

    criamos o arquivo para verificar as funcionalidades do php

---
+++ phpInfo.php
@@ -0,0 +1,19 @@
+<?php
+
+// Mostra as informações do php.
+echo phpinfo();
+
+# mata a execução do script php.
+// mata a execução do script php.
+exit;
+
+/**
+ * Este é um bloco de comentarios para o php.
+ * Este é um bloco de comentarios para o php.
+ * Este é um bloco de comentarios para o php.
+ * Este é um bloco de comentarios para o php.
+ * Este é um bloco de comentarios para o php.
+ * Este é um bloco de comentarios para o php.
+ * Este é um bloco de comentarios para o php.
+ */
+echo "ola mundo!";
