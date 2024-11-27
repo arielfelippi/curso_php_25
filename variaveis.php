116b5b8dd942ce8a2185aa8f48e1a9d3b663b4c1
arielfelippi <ariel.felippi@hotmail.com>
2024-11-26 21:35:51 -0300

    tipos de dados php

---
+++ variaveis.php
@@ -0,0 +1,21 @@
+<?php
+
+const CPF = '12345678901';
+const VELOCIDADE_DA_LUZ = '320k\s2';
+
+// tipos de dados primitivos.
+$string = "Um texto qualquer";
+$string = "joao do caminhao"; // override = substituir o que existia
+$nomePessoa = "Ariel"; // style guide Camel Case
+$nome_pessoa = "Ariel"; // style guide Snake Case
+$inteiro = 2;
+$float = 2.5;
+$double = 2.5468;
+$char = 'a';
+
+// tipos de dados estruturais
+$array = array();
+$array_short = [];
+
+$objeto = new stdClass(); // POO - OOP
+$classe = new stdClass();
