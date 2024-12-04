347903b58085d4b3f7675e06ed5c7f184ca956e2
arielfelippi <41402358+arielfelippi@users.noreply.github.com>
2024-12-03 22:00:08 -0300

    Update variaveis.php
    tabuada

--- variaveis.php
+++ variaveis.php
@@ -14,8 +14,51 @@ $double = 2.5468;
 $char = 'a';
 
 // tipos de dados estruturais
-$array = array();
-$array_short = [];
+$array = array(); // versoes antigas do php 5.0 - 7.0
+$array_short = []; // veroses mais novas do php 7.0+
 
 $objeto = new stdClass(); // POO - OOP
 $classe = new stdClass();
+
+// POO - OOP
+class CanetaAzul {
+    // Atributos e/ou Propriedades
+    public const COR = "Azul";
+    public const PLASTICO = true;
+    public $nome;
+    public $tipoMaterial = "Plastico";
+    public $dimensoes;
+    public $tipo;
+
+   // metodos e/ou funcoes
+   public function escrever() {
+
+   }
+
+   public function pintar() {
+
+   }
+}
+
+echo "Ola 
+    <b>
+        <i>
+            mundo!
+        </i>
+    </b>
+<br>"; // <br></br> = <br/> ou <br>
+
+$string = "joao do caminhao";
+echo $string; // joao do caminhao
+
+echo "<br>CPF: " . CPF;
+
+$a = 10;
+$b = 20;
+$x =  $a + $b;
+echo "<br>x: " . $x;
+
+$soma =  10 + 10;
+echo "<br>soma: " . $soma;
+
+// Tema de casa sera fazer a tabuada (6, 7) e mostra na tela.
