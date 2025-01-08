259e81caca2a76f82365715837d06d95e98e4e50
arielfelippi <41402358+arielfelippi@users.noreply.github.com>
2025-01-07 21:13:55 -0300

    Create exerciciosReforco.php

---
+++ exerciciosReforco.php
@@ -0,0 +1,11 @@
+<?php
+
+/***
+ * 1-) Recriar o exercicio da tabuada utilizando funcoes
+ * 2-) Encontrar os 10 primeiros numeros Pares e os 10 primeiros numeros impares.
+ * 3-) Encontrar os 10 primeiros numeros primos, a partir do 10.
+ * 4-) Ordenar em ordem crescente o array [10, 5, 2, 30, 85, 14].
+ *          Saída esperada: [2, 5, 10, 14, 30, 85].
+ * Não se deve utilizar funcoes nativas do php como asort, usort e sort.
+ */
+
