417368b20258024ac575db4567c4e05a31c87e1a
ariel felippi <ariel.felippi@hotmail.com>
2025-01-21 21:31:21 -0300

    ContaBancaria

---
+++ ContaBancaria.php
@@ -0,0 +1,9 @@
+<?php
+
+class ContaBancaria {
+    public function sacar() {}
+    public function depositar() {}
+    public function pix() {}
+    public function verSaldo() {}
+    public function extrato() {}
+}
