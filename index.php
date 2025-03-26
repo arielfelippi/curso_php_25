e3350c528050e962995b3668e00adfb6cd1416f9
arielfelippi <ariel.felippi@hotmail.com>
2025-03-25 21:47:44 -0300

    jquery mais tela

--- index.php
+++ index.php
@@ -9,25 +9,48 @@
 <body>
     
     <div id="resultado"></div>
+    <input type="text" id="idConta" value="0">
+    <input type="text" id="idContaDestinatario" value="0">
+    <input type="text" id="valor" value="0">
+    <button class="btn-calcular" >Calcular</button>
 
     <script>
-        const url = "http://localhost/curso_php_25/ContaBancaria.php?id=1&idDestino=3&valor=500";
-
-        $.get(url).then((contas) => {
+        $(document).ready(function() {
             
-            let htmlUl = "<ul>";
+            $(".btn-calcular").on("click", function() {
 
-            $.each(contas, function (idx, conta) {
-                    htmlUl += `<li>id: ${conta.id}</li>`;
-                    htmlUl += `<li>Nome: ${conta.nome_titular}</li>`;
-                    htmlUl += `<li>Saldo: ${conta.saldo}</li>`;
-                    htmlUl += `_____________________`;
+                const idConta = $("#idConta").val() ?? 0;
+                const idContaDestinatario = $("#idContaDestinatario").val() ?? 0;
+                const valor = $("#valor").val() ?? 0;
+                
+                const params = {
+                    id: idConta,
+                    idDestino: idContaDestinatario,
+                    valor
+                };
+        
+                var strParams = jQuery.param(params); // id=1&idDestino=3&valor=500
+        
+                const urlBase = "http://localhost/curso_php_25/ContaBancaria.php?" + strParams;
+        
+                $.get(urlBase).then((contas) => {
+                    
+                    let htmlUl = "<ul>";
+        
+                    $.each(contas, function (idx, conta) {
+                            htmlUl += `<li>id: ${conta.id}</li>`;
+                            htmlUl += `<li>Nome: ${conta.nome_titular}</li>`;
+                            htmlUl += `<li>Saldo: ${conta.saldo}</li>`;
+                            htmlUl += `_____________________`;
+                    });
+        
+                    htmlUl += "</ul>";
+                    
+                    $("#resultado").html(htmlUl);
+                });
             });
-
-            htmlUl += "</ul>";
-            
-            $("#resultado").html(htmlUl);
-        });
+            })
+    
     </script>
 </body>
 </html>
